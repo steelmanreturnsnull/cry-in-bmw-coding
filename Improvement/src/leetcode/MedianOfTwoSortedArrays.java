dca91ac4173feaf9cf4fb2783b48eae1aab71445
blackwindy2010 <blackwindy2010@gmail.com>
2013-07-23 03:32:07 +0000

    Move Directory

---
+++ Improvement/src/leetcode/MedianOfTwoSortedArrays.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/*
+ * There are two sorted arrays A and B of size m and n respectively. 
+ * Find the median of the two sorted arrays. The overall run time 
+ * complexity should be O(log (m+n)).
+*/
+
+public class MedianOfTwoSortedArrays {
+    public double findMedianSortedArrays(int A[], int B[]) {
+    	int size_a = A.length;
+    	int size_b = B.length;
+    	int start_a = 0;
+    	int end_a = size_a -1;
+    	int start_b = 0;
+    	int end_b = size_b -1;
+    	
+    	return 0.0;
+    }
+}
---
+++ Improvement/src/leetcode/PathSumII.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import common.TreeNode;
+
+public class PathSumII {
+	
+//	public static void main(String [ ] args) {
+//		pathSum(new TreeNode(1), 1);
+//	}
+	
+    public boolean isLeaf(TreeNode root) { 
+		return (root !=null && root.left ==null && root.right ==null);
+	}
+    
+    public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>> ();
+    	Stack<TreeNode> treeStack = new Stack<TreeNode> ();
+    	Stack<Integer> partialSum = new Stack<Integer> ();
+    	Stack<ArrayList<Integer>> partialPath = new Stack<ArrayList<Integer>> ();
+    	if (root == null)
+    		return result;
+    	treeStack.add(root);
+    	partialSum.add(root.val);
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		path.add(root.val);
+		partialPath.add(path);
+    	while (!treeStack.isEmpty()) {
+    		TreeNode current = treeStack.pop();
+    		int current_sum = partialSum.pop();
+    		ArrayList<Integer> current_path = partialPath.pop();
+    		if (isLeaf(current) && current_sum == sum)
+    			result.add(current_path);
+    		else {
+    			if (current.left != null) {
+    				treeStack.push(current.left);
+    				partialSum.push(current_sum + current.left.val);
+    				ArrayList<Integer> new_path = new ArrayList<Integer>(current_path);
+    				new_path.add(current.left.val);
+    				partialPath.push(new_path);
+    			}
+    			if (current.right != null) {
+    				treeStack.push(current.right);
+    				partialSum.push(current_sum + current.right.val);
+    				ArrayList<Integer> new_path = new ArrayList<Integer>(current_path);
+    				new_path.add(current.right.val);
+    				partialPath.push(new_path);
+    			}
+    		}
+    	}
+    	return result;
+    }
+}
