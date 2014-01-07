0b6f35179f372b70c36e360769706e06123e67ef
blackwindy2010@gmail.com <blackwindy2010@gmail.com@3eed7360-eb13-9778-6f5d-12dd7eea5691>
2014-01-07 23:56:54 +0000

    Casual Check in

---
+++ trunk/Improvement/src/com/leetcode/oj/array/FindSortedArrayRotation.java
@@ -0,0 +1,14 @@
+package com.leetcode.oj.array;
+
+/**
+ * Implement the following function, FindSortedArrayRotation, 
+ * which takes as its input an array of unique integers that has been sorted in ascending order, 
+ * then rotated by an unknown amount X where 0 <= X <= (arrayLength - 1). 
+ * An array rotation by amount X moves every element array[i] to array[(i + X) % arrayLength]. 
+ * FindSortedArrayRotation discovers and returns X by examining the array.
+ *
+ */
+public class FindSortedArrayRotation 
+{
+
+}
--- trunk/Improvement/src/com/leetcode/oj/array/MergeSortedArray.java
+++ trunk/Improvement/src/com/leetcode/oj/array/MergeSortedArray.java
@@ -9,19 +9,33 @@ package com.leetcode.oj.array;
  * 
  */
 
+// OK
 public class MergeSortedArray 
 {
-	 public void merge(int A[], int m, int B[], int n) 
+	// O(m+n)
+	// A little better: store in A, so we only need to move every B into A
+	public void merge(int A[], int m, int B[], int n) 
+	{
+		 while (n > 0)
+		 {
+			 if (m > 0 && A[m-1] > B[n-1])
+				 A[m + n - 1] = A[--m];
+			 else
+				 A[m + n - 1] = B[--n];
+		 }
+    }
+	 
+/*	 public void merge(int A[], int m, int B[], int n) 
 	 {
 		 m = m - 1;
 		 n = n - 1;
 		 int c = m + n - 1;
-		 while (c>0)
+		 while (c > 0)
 		 {
-			 if (m>=0 && A[m] > B[n])
+			 if (m >= 0 && A[m] > B[n])
 				 A[c--] = A[m--];
 			 else
 				 A[c--] = B[n--];
 		 }
-     }
+     }*/
 }
